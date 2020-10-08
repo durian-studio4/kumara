8749d3940e113dabb366f9d6f99c77efed2a4bfe
kuetabby <vansmuhammad@gmail.com>
2020-10-08 12:16:20 +0700

    commit

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,18 +1,42 @@
 import { Button, Result } from 'antd';
 import React from 'react';
-import { history } from 'umi';
+import { useHistory } from 'umi';
 
-const NoFoundPage: React.FC<{}> = () => (
-  <Result
-    status="404"
-    title="404"
-    subTitle="Sorry, the page you visited does not exist."
-    extra={
-      <Button type="primary" onClick={() => history.push('/')}>
-        Back Home
-      </Button>
+const NoFoundPage: React.FC<{}> = () => {
+  const history = useHistory();
+  const currentUser = localStorage.getItem('role');
+
+  const backHome = () => {
+    switch (JSON.parse(String(currentUser))) {
+      case 'Owner':
+        return history.push('/dashboard/home');
+      case 'Inventory':
+        return history.push('/inventory/stok');
+      case 'Master':
+        return history.push('/master/karyawan');
+      case 'Finance':
+        return history.push('/finance/home');
+      case 'Kurir':
+        return history.push('/kurir');
+      case 'Sales':
+        return history.push('/sales/home');
+      default:
+        return false;
     }
-  />
-);
+  };
+
+  return (
+    <Result
+      status="404"
+      title="404"
+      subTitle="Sorry, the page you visited does not exist."
+      extra={
+        <Button type="primary" onClick={backHome}>
+          Back Home
+        </Button>
+      }
+    />
+  );
+};
 
 export default NoFoundPage;
